4bff4d17ba178f85cd3597999e6e423fdce78f30
Csonka-Kis Andrea <cskand@gmail.com>
2019-08-13 07:01:28 +0200

    inserted final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,7 @@
-// import { Component, OnInit, OnDestroy } from '@angular/core';
-// import { UserService } from './service/user.service';
-// import { User } from './model/user';
-// import { Subscription } from 'rxjs';
-
-// @Component({
-//   selector: 'app-root',
-//   templateUrl: './app.component.html',
-//   styleUrls: ['./app.component.css']
-// })
-// export class AppComponent implements OnInit, OnDestroy {
-//   title = 'Data Table';
-//   userList: User[];
-//   userSubscription: Subscription;
-
-//   constructor(
-//     private userService: UserService
-//   ) {
-
-//   }
-
 import { Component } from '@angular/core';
+import { UserService } from './service/user.service';
+import { User } from './model/user';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -27,7 +9,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+  title = 'Data Table';
 }
 
 //   ngOnInit() {
